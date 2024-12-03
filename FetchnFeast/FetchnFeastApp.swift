89fb352c5e7692a6ecdeb72533c9318469a3c5e3
David Nguyen <dlngu2728@gmail.com>
2024-12-03 11:21:32 -0500

    Initial Commit

---
+++ FetchnFeast/FetchnFeastApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchnFeastApp.swift
+//  FetchnFeast
+//
+//  Created by David Nguyen on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchnFeastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
